745d0d8007722703d8cc5574e8e010a947c21326
vitorbruski <vitorgbruski@gmail.com>
2023-11-16 17:59:35 -0300

    [display-date] criado

---
+++ services/display-date.ts
@@ -0,0 +1,59 @@
+type DayType =
+  | 1
+  | 2
+  | 3
+  | 4
+  | 5
+  | 6
+  | 7
+  | 8
+  | 9
+  | 10
+  | 11
+  | 12
+  | 13
+  | 14
+  | 15
+  | 16
+  | 17
+  | 18
+  | 19
+  | 20
+  | 21
+  | 22
+  | 23
+  | 24
+  | 25
+  | 26
+  | 27
+  | 28
+  | 29
+  | 30
+  | 31
+
+type MonthType = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
+
+type DateType = {
+  day: DayType
+  month: MonthType
+  year: number
+}
+
+function displayDate(date: DateType) {
+  const months = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ]
+
+  return `${date.day} de ${months[date.month - 1]} de ${date.year}`
+}
