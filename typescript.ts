3469079700f4682cbf1093a3a73d248f761616c8
vitorbruski <vitorgbruski@gmail.com>
2024-02-12 15:45:20 -0300

    update

--- typescript.ts
+++ typescript.ts
@@ -1,10 +1,8 @@
 /* hooks */
-
 export { default as useClassName } from "./typescript/hooks/use-classname"
 export { default as useDefaults } from "./typescript/hooks/use-defaults"
 
 /* helpers */
-
 export { default as getAttribute } from "./typescript/helpers/document/get-attribute"
 export { default as setAttribute } from "./typescript/helpers/document/set-attribute"
 
@@ -14,10 +12,8 @@ export { default as getItem } from "./typescript/helpers/localstorage/get-item"
 export { default as setItem } from "./typescript/helpers/localstorage/set-item"
 
 /* modules */
-
 export { default as changeTheme } from "./typescript/modules/change-theme"
 export { default as getBrowserTheme } from "./typescript/modules/get-browser-theme"
 
 /* services */
-
 export { default as displayDate, type DateType } from "./typescript/services/display-date"
